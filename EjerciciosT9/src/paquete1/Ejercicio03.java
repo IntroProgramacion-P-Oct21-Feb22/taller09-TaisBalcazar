6d53ccfb8e6e93f565797001de192de75c636fc0
John Doe <johndoe@example.com>
2021-12-17 11:49:28 -0500

    primer commit

--- EjerciciosT9/src/paquete1/Ejercicio03.java
+++ EjerciciosT9/src/paquete1/Ejercicio03.java
@@ -7,7 +7,7 @@ package paquete1;
 
 /**
  *
- * @author reroes
+ * @author TaisBalcazar
  */
 public class Ejercicio03 {
 
@@ -16,10 +16,29 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        int elementosArribaMedia;
-        int elementosAbajoMedia;
-        
-    }
+        double[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
+        int elementosArribaMedia = 0;
+        int elementosAbajoMedia = 0;
+        double suma = 0;
+        double media;
 
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+        }
+
+        media = suma / arreglo.length;
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] > media) {
+                elementosArribaMedia = elementosArribaMedia + 1;
+            } else {
+                elementosAbajoMedia = elementosAbajoMedia + 1;
+            }
+
+        }
+        System.out.printf("Elementos arriba de la media: %s\n"
+                + "Elementos abajo de la media: %s\n",
+                elementosArribaMedia, 
+                elementosAbajoMedia);
+
+    }
 }
